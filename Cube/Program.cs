b9172dee58e2daa94515af3a1351bee2a6219654
Zch0 <3221458901@qq.com>
2025-03-20 18:46:23 +0800

    添加项目文件。

---
+++ Cube/Program.cs
@@ -0,0 +1,145 @@
+﻿class Cube
+{
+    public int width {  get; set; }
+    public int height { get; set; }
+    public int length { get; set; }
+    public Screen screen { get; }
+    private readonly int centerX, centerY;
+    private double angleX, angleY, angleZ;
+    private double angleX_increment, angleY_increment, angleZ_increment;
+    private double fps;
+    private int[,] vertices;
+    private int[,] edges;
+
+    public Cube(int width,int height,int length)
+    {
+        this.width = width;
+        this.height = height;
+        this.length = length/2;
+        this.screen = new Screen(width, height);
+        this.centerX = width / 2;
+        this.centerY = height / 2;
+        this.angleX = 0;
+        this.angleY = 0;
+        this.angleZ = 0;
+        this.angleX_increment = 1.0;
+        this.angleY_increment = 1.0;
+        this.angleZ_increment = 1.0;
+        this.fps = 60;
+        this.vertices = new int[8, 3] {
+            { -1, -1, -1 },
+            { 1, -1, -1 },
+            { 1, 1, -1 },
+            { -1, 1, -1 },
+            { -1, -1, 1 },
+            { 1, -1, 1 },
+            { 1, 1, 1 },
+            { -1, 1, 1 }
+        };
+        this.vertices = FormatVertices(this.vertices,this.length);
+        this.edges = new int[12, 2] {
+            { 0, 1 },
+            { 1, 2 },
+            { 2, 3 },
+            { 3, 0 },
+            { 4, 5 },
+            { 5, 6 },
+            { 6, 7 },
+            { 7, 4 },
+            { 0, 4 },
+            { 1, 5 },
+            { 2, 6 },
+            { 3, 7 }
+        };
+    }
+    private int[,] FormatVertices(int[,] vertices,int lenth)
+    {
+        int rows = vertices.GetLength(0);
+        int cols = vertices.GetLength(1);
+        int[,] new_vertices = new int[rows, cols];
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                new_vertices[i, j] = vertices[i, j] * length;
+            }
+        }
+        return new_vertices;
+    }
+    static private (double,double) RotateX(double y, double z, double angle)
+    {
+        double rad = angle * Math.PI / 180;
+        double new_y = y * Math.Cos(rad) - z * Math.Sin(rad);
+        double new_z = y * Math.Sin(rad) + z * Math.Cos(rad);
+        return (new_y, new_z);
+    }
+    static private (double,double) RotateY(double x, double z, double angle)
+    {
+        double rad = angle * Math.PI / 180;
+        double new_x = x * Math.Cos(rad) - z * Math.Sin(rad);
+        double new_z = x * Math.Sin(rad) + z * Math.Cos(rad);
+        return (new_x, new_z);
+    }
+    static private (double,double) RotateZ(double x, double y, double angle)
+    {
+        double rad = angle * Math.PI / 180;
+        double new_x = x * Math.Cos(rad) - y * Math.Sin(rad);
+        double new_y = x * Math.Sin(rad) + y * Math.Cos(rad);
+        return (new_x, new_y);
+    }
+    static public (double,double) Deconstruct_2D(double[] arr)
+    {
+        return (arr[0], arr[1]);
+    }
+    private void AutoRotate()
+    {
+        this.angleX += this.angleX_increment;
+        this.angleY += this.angleY_increment;
+        this.angleZ += this.angleZ_increment;
+        this.angleX = this.angleX % 360;
+        this.angleY = this.angleY % 360;
+        this.angleZ = this.angleZ % 360;
+    }
+    public void Draw()
+    {
+        //Thread.Sleep((int)(1000 / this.fps));
+        this.screen.clear_buffer();
+        this.screen.refresh_screen();
+        this.AutoRotate();
+        for (int i = 0; i < this.edges.GetLength(0); i++)
+        {
+            double x1= this.vertices[this.edges[i, 0], 0];
+            double y1 = this.vertices[this.edges[i, 0], 1];
+            double z1 = this.vertices[this.edges[i, 0], 2];
+            double x2 = this.vertices[this.edges[i, 1], 0];
+            double y2 = this.vertices[this.edges[i, 1], 1];
+            double z2 = this.vertices[this.edges[i, 1], 2];
+            (y1, z1) = RotateX(y1, z1, this.angleX);
+            (x1, z1) = RotateY(x1, z1, this.angleY);
+            (x1, y1) = RotateZ(x1, y1, this.angleZ);
+            (y2, z2) = RotateX(y2, z2, this.angleX);
+            (x2, z2) = RotateY(x2, z2, this.angleY);
+            (x2, y2) = RotateZ(x2, y2, this.angleZ);
+            this.screen.draw_line(x1 + this.centerX, y1 + this.centerY, x2 + this.centerX, y2 + this.centerY, true);
+        }
+    }
+}
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Cube cube = new Cube(256, 256, 128);
+        while (true)
+        {
+            cube.Draw();
+            cube.screen.display();
+        }
+        //for (int i = 0; i < 100; i++)
+        //{
+        //    Thread.Sleep(100);
+        //    Console.WriteLine(i);
+        //    cube.screen.refresh_screen();
+        //}
+    }
+}
---
+++ Cube/draw_in_terminal.cs
@@ -0,0 +1,151 @@
+﻿using System;
+
+public class Screen
+{
+    public int width { get; set; }
+    public int height { get; set;}
+
+    private bool[,] buffer;
+    public Screen(int width, int height)
+	{
+		this.width = width;
+        this.height = height;
+        this.buffer = new bool[width, height];
+        Console.CursorVisible = false;
+    }
+    public void clear_buffer()
+    {
+        this.buffer = new bool[this.width, this.height];
+    }
+    public void refresh_screen()
+    {
+        Console.SetCursorPosition(0, 0);
+    }
+    public void clear_screen()
+    {
+        Console.Clear();
+    }
+    public void set_pixel(double x, double y, bool value)
+    {
+        if (x < 0 || x >= this.width || y < 0 || y >= this.height)
+        {
+            Console.WriteLine($"({x},{y})超界！！！");
+            return;
+        }
+        this.buffer[(int)x, (int)y] = value;
+    }
+    public void set_pixels(double x1, double y1, double x2, double y2, bool value)
+    {
+        for (int x = (int)x1; x <= x2; x++)
+        {
+            for (int y = (int)y1; y <= y2; y++)
+            {
+                this.buffer[x, y] = value;
+            }
+        }
+    }
+    public void draw_line(double x1, double y1, double x2, double y2, bool value)
+    {
+        double kY = (x1 != x2) ? (double)(y2 - y1) / (x2 - x1) : double.PositiveInfinity;
+        double kX = (y1 != y2) ? (double)(x2 - x1) / (y2 - y1) : double.PositiveInfinity;
+
+        // 定义 funcY 和 funcX 方法
+        Func<int, int> funcY = (x) => (int)(kY * (x - x1) + y1);
+        Func<int, int> funcX = (y) => (int)(kX * (y - y1) + x1);
+
+        // 斜率不存在时判断
+        if (double.IsPositiveInfinity(kY))
+        {
+            if (y1 <= y2)
+            {
+                for (int y = (int)y1; y <= y2; y++)
+                {
+                    this.set_pixel(x1, y, value);
+                }
+            }
+            else
+            {
+                for (int y = (int)y2; y <= y1; y++)
+                {
+                    this.set_pixel(x1, y, value);
+                }
+            }
+        }
+        else if (double.IsPositiveInfinity(kX))
+        {
+            if (x1 <= x2)
+            {
+                for (int x = (int)x1; x <= x2; x++)
+                {
+                    this.set_pixel(x, y1, value);
+                }
+            }
+            else
+            {
+                for (int x = (int)x2; x <= x1; x++)
+                {
+                    this.set_pixel(x, y1, value);
+                }
+            }
+        }
+        else
+        {
+            // 当斜率小于 1 时，以 x 为自变量遍历
+            if (Math.Abs(kY) < 1)
+            {
+                if (x1 <= x2)
+                {
+                    for (int x = (int)x1; x <= x2; x++)
+                    {
+                        this.set_pixel(x, funcY(x), value);
+                    }
+                }
+                else
+                {
+                    for (int x = (int)x2; x <= x1; x++)
+                    {
+                        this.set_pixel(x, funcY(x), value);
+                    }
+                }
+            }
+            // 当斜率大于等于 1 时，以 y 为自变量遍历
+            else
+            {
+                if (y1 <= y2)
+                {
+                    for (int y = (int)y1; y <= y2; y++)
+                    {
+                        this.set_pixel(funcX(y), y, value);
+                    }
+                }
+                else
+                {
+                    for (int y = (int)y2; y <= y1; y++)
+                    {
+                        this.set_pixel(funcX(y), y, value);
+                    }
+                }
+            }
+        }
+    }
+    public void display()
+    {
+        System.Text.StringBuilder buffer_str = new System.Text.StringBuilder();
+        for (int y = 0; y < this.height; y++)
+        {
+            for (int x = 0; x < this.width; x++)
+            {
+                if (this.buffer[x, y])
+                {
+                    buffer_str.Append("██");
+                }
+                else
+                {
+                    buffer_str.Append("  ");
+                }
+            }
+            buffer_str.Append('\n');
+        }
+        Console.Write(buffer_str.ToString());
+    }
+}
